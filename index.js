2b023d8105aab4258ac871f2ff7c7481a06e1ec2
javoh1rqaxramonov <ubotirov22@.com>
2024-09-29 21:42:37 +0500

    finished

---
+++ index.js
@@ -0,0 +1,315 @@
+// 1-masala
+
+// let son = +prompt("sonni kiriting");
+// if (son>0) {
+//     son++;
+// }
+// console.log(son);
+
+// 2-masala 
+
+// let son = +prompt("sonni kiriting");
+// if (son>0) {
+//     son++;
+// } else{
+//     son-=2
+// }
+// console.log(son);
+
+// 3-masala 
+// let son = +prompt("sonni kiriting");
+// if (son>0) {
+//     son++;
+// } 
+// if(son<0){
+//     son-=2;
+// }
+// if (son==0) {
+//     son+=10;
+    
+// }
+// console.log(son);
+
+// 4-masala 
+
+// let son1 = +prompt("1-sonni kiriting") ;
+// let son2 = +prompt("2-sonni kiriting");
+// let son3 = +prompt("3-sonni kiriting");
+// let counter =0;
+// if (son1>0) {
+//     counter++;
+// }
+// if (son2>0) {
+//     counter++;
+// }
+// if (son3>0) {
+//     counter++;
+// }
+// console.log(counter);
+
+// 5-masala 
+
+// let son1 = +prompt("1-sonni kiriting") ;
+// let son2 = +prompt("2-sonni kiriting");
+// let son3 = +prompt("3-sonni kiriting");
+// let counter =0;
+// let sum =0;
+
+// if (son1>0) {
+//     counter++;
+// } else{
+//     sum++;
+// }
+// if (son2>0) {
+//     counter++;
+// } else{
+//     sum++;
+// }
+// if (son3>0) {
+//     counter++;
+// } else{
+//     sum++;
+// }
+// console.log(counter);
+// console.log(sum);
+
+// 6-masala 
+
+// let son1 = +prompt("1-sonni kiriting") ;
+// let son2 = +prompt("2-sonni kiriting");
+// let max=0;
+// if (son1>son2) {
+//     max=son1;
+// } else {
+//     max=son2;
+// }
+// console.log(max);
+
+// 7-masala 
+// 8-masala 
+
+// let son1 = +prompt("1-sonni kiriting") ;
+// let son2 = +prompt("2-sonni kiriting");
+// let min=0;
+// let max=0;
+// if (son1>son2) {
+//     max=son1;
+// } else {
+//     max=son2;
+// }
+// if (son1<son2) {
+//     min=son1;
+// } else {
+//     min=son2;
+// }
+// console.log(max);
+// console.log(min);
+
+// 9-masala 
+
+// let a=+prompt("a-sonni kiriting");
+// let b=+prompt("b-sonni kiriting");
+// if (a<b) {
+//     a+=b;
+// } else{
+//     b+=a;
+// }
+// console.log(a,b);
+
+// 10-masala
+
+// let son1 = +prompt("a-sonni kiriting");
+// let son2 = +prompt("b-sonni kiriting");
+// let sum=0;
+// if (son1!=son2) {
+//     sum=son1+son2;
+// } else {
+//     sum==0;
+// }
+// console.log(sum);
+
+// 11-masala
+// let son1 = +prompt("a-sonni kiriting");
+// let son2 = +prompt("b-sonni kiriting");
+// let sum=0;
+// let max=0;
+// if (son1!=son2) {
+//     if (son1>son2) {
+//         max=son1;
+//     } else {
+//         max=son2;
+//     }
+// } 
+// if (son1==son2) {
+//     sum==0;
+// }
+// console.log(sum);
+// console.log(son2);
+// console.log(son1);
+// console.log(max);
+
+// 12-masala 
+// let son1 = +prompt("1-sonni kiriting");
+// let son2 = +prompt("2-sonni kiriting");
+// let son3 = +prompt("3-sonni kiriting");
+// let min=0;  
+// if (son1>son2) {
+//     min=son2;
+// } else{
+//     min=son1;
+// }
+// if (min>son3) {
+//     min=son3;
+// }
+// console.log(min);
+
+// 13-masala 
+// 14-masala 
+
+// let son1 = +prompt("1-sonni kiriting");
+// let son2 = +prompt("2-sonni kiriting");
+// let son3 = +prompt("3-sonni kiriting");
+// let min=0;
+// let max=0;
+// if (son1>son2) {
+//     max=son1;
+// } else {
+//     min=son2;
+// }
+// if (son1<son2) {
+//     min=son1;
+// } else {
+//     max=son2;
+// }
+// if (max<son3 ) {
+//     max=son3
+// } else{
+//     min=max;
+// }
+    
+// console.log(min);
+// console.log(max);
+
+// 15-masala 
+// 1-masala
+// let son = +prompt("sonni kiriting");
+// if (son>0) {
+//     console.log(true);
+    
+// } else{
+//     console.log(false);
+    
+// }
+
+// 2-masala 
+// let son = +prompt("sonni kiriting");
+// if (son%2==1) {
+//     console.log(true);
+    
+// } else{
+//     console.log(false);
+    
+// }
+
+// 3-masala 
+// let son = +prompt("sonni kiriting");
+// if (son%2==0) {
+//     console.log(true);
+    
+// } else{
+//     console.log(false);
+    
+// }
+
+// 4-masala 
+// let son1 = +prompt("1-sonni kiriting");
+// let son2 = +prompt("2-sonni kiriting");
+
+// if (son1>2 && son2<=3) {
+//     console.log(true);
+    
+// } else{
+//     console.log(false);
+    
+// }
+
+// 5-masala
+// let son1 = +prompt("1-sonni kiriting");
+// let son2 = +prompt("2-sonni kiriting");
+
+// if (son1>=0 && son2<-2) {
+//     console.log(true);
+    
+// } else{
+//     console.log(false);
+    
+// }
+
+// 6-masala 
+// let son1 = +prompt("1-sonni kiriting");
+// let son2 = +prompt("2-sonni kiriting");
+// let son3 = +prompt("3-sonni kiriting");
+
+// if(son1<=son2 && son2<=son3){
+//     console.log(true);
+    
+// } else{
+//     console.log(false);
+    
+// }
+
+// 7-masala 
+// let son1 = +prompt("1-sonni kiriting");
+// let son2 = +prompt("2-sonni kiriting");
+// let son3 = +prompt("3-sonni kiriting");
+
+// if(son1>=son2 && son2<=son3){
+//     console.log(true);
+    
+// } else{
+//     console.log(false);
+    
+// }
+
+// 8-masala 
+// let son1 = +prompt("1-sonni kiriting");
+// let son2= +prompt("2-sonni kiriting");
+
+// if (son1%2==1 && son2%2==1) {
+//     console.log(true);
+    
+// } else{
+//     console.log(false);
+    
+// }
+
+// 9-masala 
+// let son1 = +prompt("1-sonni kiriting");
+// let son2= +prompt("2-sonni kiriting");
+
+// if (son1%2==1 || son2%2==1) {
+//     console.log(true);
+    
+// } else{
+//     console.log(false);
+    
+// }
+
+// 10-masala 
+// let son1 = +prompt("1-sonni kiriting");
+// let son2= +prompt("2-sonni kiriting");
+// let counter = 0;
+// if (son1%2==1 || son2%2==1) {
+//     console.log(true);
+    
+// } else{
+//     console.log(false);
+    
+// }
+// if (counter>1) {
+//     console.log(true);
+    
+// } else {
+//     console.log(false);
+    
+// }
